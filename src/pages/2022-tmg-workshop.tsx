89db3874fb7699ecf06a37ed5caec2786af4e39b
Mirko Lenz <info@mirko-lenz.de>
2022-04-12 08:53:33 +0200

    Reformat workshop content

--- src/pages/2022-tmg-workshop.tsx
+++ src/pages/2022-tmg-workshop.tsx
@@ -24,25 +24,17 @@ interface Props {
 
 const Page: React.FC<Props> = ({ data }) => (
   <Layout title="Workshop on Text Mining and Generation (TMG) at KI-2022">
+    {/* prettier-ignore */}
     <P>
-      Digital text data is available in large amounts and different
-      granularities. Typical sources include social media posts, books, news
-      articles, web pages or company reports, etc. A major challenge this text
-      data imposes is that it is unstructured and must first be processed to
-      make further analysis possible. At the same time, there are also many
-      situations in which only structured data is available that is to be
-      verbally explained—for instance, by Explainable AI. These contrasting
-      scenarios lead to two complementary application areas: text mining and
-      text generation. The aim of text mining is to analyze the content of
-      unstructured text and extract (useful) structured information. In
-      contrast, text generation attempts to (automatically) create text from
-      structured information or knowledge that is for example stored in large
-      language models. The goal of the TMG workshop is to bring these two
-      perspectives together by eliciting research paper submissions that aim for
-      bridging the gap between knowledge extraction and text generation. Since
-      recent approaches to text mining and text generation are predominantly
-      based on artificial intelligence (AI) methodologies, KI 2022 is a relevant
-      venue to bring together AI researchers working on these two tasks.
+      Digital text data is available in large amounts and different granularities.
+      Typical sources include social media posts, books, news articles, web pages or company reports, etc.
+      A major challenge this text data imposes is that it is unstructured and must first be processed to make further analysis possible.
+      At the same time, there are also many situations in which only structured data is available that is to be verbally explained—for instance, by Explainable AI.
+      These contrasting scenarios lead to two complementary application areas: text mining and text generation.
+      The aim of text mining is to analyze the content of unstructured text and extract (useful) structured information.
+      In contrast, text generation attempts to (automatically) create text from structured information or knowledge that is for example stored in large language models.
+      The goal of the TMG workshop is to bring these two perspectives together by eliciting research paper submissions that aim for bridging the gap between knowledge extraction and text generation.
+      Since recent approaches to text mining and text generation are predominantly based on artificial intelligence (AI) methodologies, KI 2022 is a relevant venue to bring together AI researchers working on these two tasks
     </P>
 
     <H2>Important Dates</H2>
@@ -59,27 +51,23 @@ const Page: React.FC<Props> = ({ data }) => (
     />
 
     <H2>Call for Papers</H2>
+    {/* prettier-ignore */}
     <P>
-      We welcome any submissions that deal with transforming the representation
-      of data using techniques of natural language processing (NLP): (applied)
-      research papers, theoretical papers, user studies or prospective papers.
+      We welcome any submissions that deal with transforming the representation of data using techniques of natural language processing (NLP):
+      (applied) research papers, theoretical papers, user studies or prospective papers.
       Topics include, but are not limited to, the following:
     </P>
+    {/* prettier-ignore */}
     <Ul>
       <Li>Answer generation for question answering.</Li>
       <Li>Argument mining.</Li>
-      <Li>
-        Ethical aspects of AI for text generation (e.g., mitigating bias,
-        misinformation, etc.).
-      </Li>
+      <Li>Ethical aspects of AI for text generation (e.g., mitigating bias,misinformation, etc.).</Li>
       <Li>Generating descriptions for graph-based workflows.</Li>
       <Li>Generating explanations in recommender systems.</Li>
       <Li>Graph-to-text generation for knowledge graphs.</Li>
       <Li>Methods for Explainable AI.</Li>
       <Li>Information extraction.</Li>
-      <Li>
-        Knowledge graph refinement, particularly featuring text-based signals.
-      </Li>
+      <Li>Knowledge graph refinement, particularly featuring text-based signals.</Li>
       <Li>Parsing argumentative structures in texts.</Li>
       <Li>Pattern detection in log files.</Li>
       <Li>Snippet generation for search results.</Li>
@@ -98,17 +86,9 @@ const Page: React.FC<Props> = ({ data }) => (
           alt="Prof. Dr. Iryna Gurevych"
         />
       </Box>
+      {/* prettier-ignore */}
       <P>
-        Pariatur ipsum consectetur in qui Lorem culpa in deserunt amet. Aliquip
-        eu voluptate nisi in ad sunt enim veniam et. Quis veniam ad veniam.
-        Velit nostrud ipsum labore pariatur nulla ea aliquip in incididunt
-        officia ipsum dolor cupidatat. Do aliqua veniam dolore. Duis velit do ea
-        nostrud ad quis elit nostrud quis cupidatat eu laboris. Eiusmod nulla
-        excepteur commodo tempor minim est reprehenderit laborum proident amet
-        incididunt. Dolor tempor voluptate commodo occaecat cupidatat aute
-        commodo voluptate reprehenderit ullamco elit eu dolor occaecat deserunt.
-        Elit deserunt ut fugiat sunt consectetur pariatur. Aute id et in ex sunt
-        anim officia sit irure anim labore velit.
+        Dolor minim nisi mollit sit incididunt labore enim deserunt fugiat consequat qui nisi. Est eiusmod dolor deserunt qui culpa cillum nisi dolor adipisicing duis. Laborum exercitation fugiat laboris exercitation minim nostrud in eu consequat. Mollit nisi nostrud consectetur ipsum ex ex anim elit. Occaecat irure qui ea nostrud excepteur velit anim nulla proident consectetur qui. Pariatur aute duis laborum aute irure culpa aliqua eu laboris et est officia anim. Dolor cupidatat ea ullamco ipsum veniam. Sunt sint enim velit cillum irure ad et ipsum laborum deserunt. Dolor sunt reprehenderit aliqua minim velit aliqua. Ullamco qui laborum officia sint non pariatur sint.
       </P>
     </SimpleGrid>
 
