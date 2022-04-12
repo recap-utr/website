d539621d0e93d191bcd2da28df2971b8680edbee
Mirko Lenz <info@mirko-lenz.de>
2022-04-12 10:43:58 +0200

    Use blurred placeholder

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,6 +24,7 @@ const config: GatsbyConfig = {
       options: {
         defaults: {
           formats: ["auto", "webp"],
+          placeholder: "blurred",
           quality: 50,
         },
       },
