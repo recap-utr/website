442fddf675b89dac6be739af2ee30a0987a8923e
Mirko Lenz <info@mirko-lenz.de>
2022-04-13 10:43:06 +0200

    Enable support for github icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,9 +1,14 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { IconName } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const Icon: React.FC<{ name: IconName }> = ({ name }) => (
-  <FontAwesomeIcon icon={["fas", name as IconName]} />
-);
+const Icon: React.FC<{ name: IconName | "github" }> = ({ name }) => {
+  if (name === "github") {
+    return <FontAwesomeIcon icon={faGithub} />;
+  } else {
+    return <FontAwesomeIcon icon={["fas", name]} />;
+  }
+};
 
 export default Icon;
