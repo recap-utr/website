4969ceb49e49028c3792174785e08b55c6e85012
Mirko Lenz <info@mirko-lenz.de>
2022-04-19 14:57:01 +0200

    Create slug for markdown pages

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
 import Cite from "citation-js";
+import { GatsbyNode } from "gatsby";
+import { createFilePath } from "gatsby-source-filesystem";
 
 // https://github.com/tetsuyakanda/gatsby-transformer-citationjs/blob/letsgo/src/gatsby-node.js
 function format(entry: { [key: string]: any }) {
@@ -7,39 +9,43 @@ function format(entry: { [key: string]: any }) {
   return entry;
 }
 
-export async function onCreateNode({
+export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
   node,
   actions,
   loadNodeContent,
   createNodeId,
   createContentDigest,
-}) {
-  // file type check
-  if (node.extension !== `bib`) {
-    return;
-  }
+  getNode,
+}) => {
+  const { createNode, createParentChildLink, createNodeField } = actions;
 
-  const { createNode, createParentChildLink } = actions;
+  if (node.extension === `bib`) {
+    const content = await loadNodeContent(node);
+    const parsedContent = new Cite(content).data;
 
-  const content = await loadNodeContent(node);
-  const parsedContent = new Cite(content).data;
-
-  parsedContent
-    .map(format)
-    .map((d) => {
-      return {
-        ...d,
-        id: createNodeId(`${node.id} ${d.id} >>> Citation`),
-        children: [],
-        parent: node.id,
-        internal: {
-          contentDigest: createContentDigest(d),
-          type: `Citation`,
-        },
-      };
-    })
-    .forEach((data) => {
-      createNode(data);
-      createParentChildLink({ parent: node, child: data });
+    parsedContent
+      .map(format)
+      .map((d) => {
+        return {
+          ...d,
+          id: createNodeId(`${node.id} ${d.id} >>> Citation`),
+          children: [],
+          parent: node.id,
+          internal: {
+            contentDigest: createContentDigest(d),
+            type: `Citation`,
+          },
+        };
+      })
+      .forEach((data) => {
+        createNode(data);
+        createParentChildLink({ parent: node, child: data });
+      });
+  } else if (node.internal.type === "MarkdownRemark") {
+    createNodeField({
+      node,
+      name: "slug",
+      value: createFilePath({ node, getNode }),
     });
-}
+  }
+};
