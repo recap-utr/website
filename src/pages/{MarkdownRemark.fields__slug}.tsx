18a6d0837aaf6c77ba8f29d2034b162acc8a116a
Mirko Lenz <info@mirko-lenz.de>
2022-04-19 14:57:14 +0200

    Add markdown template

---
+++ src/pages/{MarkdownRemark.fields__slug}.tsx
@@ -0,0 +1,64 @@
+import { graphql } from "gatsby";
+import React, { createElement, Fragment } from "react";
+import rehypeReact from "rehype-react";
+import { unified } from "unified";
+import * as body from "../components/BodyComponents";
+import Layout from "../components/Layout";
+
+const processor = unified().use(rehypeReact, {
+  createElement,
+  Fragment,
+  components: {
+    h2: body.H2,
+    h3: body.H3,
+    h4: body.H4,
+    h5: body.H5,
+    h6: body.H6,
+    a: body.A,
+    p: body.P,
+    li: body.Li,
+    ul: body.Ul,
+    ol: body.Ol,
+    hr: body.Hr,
+  } as any,
+});
+
+const renderAst = (ast: any): JSX.Element => {
+  return processor.stringify(ast) as unknown as JSX.Element;
+};
+
+export interface Props {
+  data: {
+    page: {
+      htmlAst: any;
+      frontmatter: {
+        title: string;
+        description?: string;
+      };
+    };
+  };
+}
+
+const Page: React.FC<Props> = ({ data }) => {
+  const { page } = data;
+  const { frontmatter, htmlAst } = page;
+  return (
+    <Layout title={frontmatter.title} description={frontmatter.description}>
+      {renderAst(htmlAst)}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        description
+      }
+      htmlAst
+    }
+  }
+`;
+
+export default Page;
