0d4abf61a68fee0dd53a2a34a9508f315aa35779
Mirko Lenz <mirko@mirkolenz.com>
2023-01-15 13:02:51 +0100

    fix: redirects not working

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -11,7 +11,12 @@ export const createPages: GatsbyNode["createPages"] = async ({ actions }) => {
   };
 
   Object.entries(redirects).forEach(([fromPath, toPath]) => {
-    createRedirect({ fromPath, toPath });
+    createRedirect({
+      fromPath,
+      toPath,
+      isPermanent: true,
+      redirectInBrowser: true,
+    });
   });
 };
 
