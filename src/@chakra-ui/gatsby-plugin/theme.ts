b408c6e95df4ad204b71687e5e36fbf62fb902ce
Mirko Lenz <mirko@mirkolenz.com>
2023-07-09 21:29:12 +0200

    fix: make links colored

--- src/@chakra-ui/gatsby-plugin/theme.ts
+++ src/@chakra-ui/gatsby-plugin/theme.ts
@@ -1,8 +1,23 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import {
+  extendTheme,
+  type ComponentSingleStyleConfig,
+  type ThemeConfig,
+} from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "system",
   useSystemColorMode: true,
 };
 
-export default extendTheme({ config });
+const Link: ComponentSingleStyleConfig = {
+  baseStyle: {
+    color: "teal.500",
+  },
+};
+
+export default extendTheme({
+  config,
+  components: {
+    Link,
+  },
+});
